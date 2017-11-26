4469617f40a6f51f425604ad2c66ea2142af6dfb
Tal Wrii <talwrii@gmail.com>
2017-11-26 21:40:25 +0100

    remove meaningless comments

--- rofimem/rofimem.py
+++ rofimem/rofimem.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import argparse
 import contextlib
-# make code as python 3 compatible as possible
 
 import os
 import subprocess
